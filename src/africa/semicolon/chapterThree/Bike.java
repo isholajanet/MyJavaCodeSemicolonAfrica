3245819e668fb6ea03c4a489aca75b5804c816f7
Janet Ishola <janetishola2010@gmail.com>
2021-05-15 23:30:28 +0100

    updating the requirement that my 5 gears changes

--- src/africa/semicolon/chapterThree/Bike.java
+++ src/africa/semicolon/chapterThree/Bike.java
@@ -3,15 +3,19 @@ package africa.semicolon.chapterThree;
 public class Bike {
     private int speedInKmPerHour;
     private boolean isOn;
-    public Bike(boolean isEngineOn, int OurDefaultSpeedInKmPerHour){
-        isOn = isEngineOn;
-        speedInKmPerHour = OurDefaultSpeedInKmPerHour;
+    private int gearInMotion;
 
-    }
+    /**
+     * public Bike(boolean isEngineOn, int OurDefaultSpeedInKmPerHour){
+     * isOn = isEngineOn;
+     * speedInKmPerHour = OurDefaultSpeedInKmPerHour;
+     * <p>
+     * }
+     **/
 
 
     public void setSpeedInKmPerHour(int newSpeedInKmPerHour) {
-        if(isOn) {
+        if (isOn) {
             speedInKmPerHour = newSpeedInKmPerHour;
         }
     }
@@ -26,7 +30,31 @@ public class Bike {
 
 
     public void setOn(boolean isEngineOn) {
-        //this.isOn = isEngineOn;
+        this.isOn = isEngineOn;
 
     }
+
+    public void speedUpBike() {
+
+   }
+
+    public void setGear(int gear) {
+        if(isOn && speedInKmPerHour > 0 && speedInKmPerHour <= 60){
+            gear = 1;
+        }else if (isOn && speedInKmPerHour > 61 && speedInKmPerHour <= 120){
+            gear = 2;
+        }else if (isOn && speedInKmPerHour > 121 && speedInKmPerHour <= 180){
+            gear = 3;
+        }else if (isOn && speedInKmPerHour > 181 && speedInKmPerHour <= 240){
+            gear = 4;
+        }else if (isOn && speedInKmPerHour > 241 && speedInKmPerHour <= 300){
+            gear = 5;
+        }
+        this.gearInMotion = gear;
+    }
+
+    public int getGear() {
+        return gearInMotion;
+    }
 }
+
--- test/africa/semicolon/chapterThree/BikeTest.java
+++ test/africa/semicolon/chapterThree/BikeTest.java
@@ -1,3 +1,11 @@
+/** The requirement for my bike is: it has 5 gears, the maximum speed is 300km/h it should change to gear 2
+ * if the speed is above 60km/h / change to gear 3 is the speed is above 120km/h / change to gear 4 is above 180km
+ * /change to gear 5 if the speed is above 240km/h
+ * i.e gear 1 - 1km/h - 60km/h
+ * gear 2 - 61km/h - 120km/h
+ * gear 3 - 121km/h - 180km/h
+ * gear 4 - 181km/h - 240km/h
+ * gear 5 - 241km/h - 300km/h  **/
 package africa.semicolon.chapterThree;
 
 //import org.junit.jupiter.api.BeforeEach;
@@ -8,25 +16,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BikeTest {
     private Bike okada;
-//    @BeforeEach
-//    public void startEachTestWithThis(){
-//        okada = new Bike(false, 45);
-//    }
+
     @BeforeEach
     public void startEachTestWithThis(){
-        okada = new Bike(false, 45);
+        okada = new Bike();
     }
 
     @Test
     public void testThatSpeedCanBeSet(){
         //given
-        Bike okada =  new Bike(false, 45);
+        Bike okada =  new Bike();
+        okada.setOn(false);
         assertFalse(okada.isOn());
-        Bike janetBike = new Bike(true, 63);
+        Bike janetBike = new Bike();
+        janetBike.setOn(true);
         assertTrue(janetBike.isOn());
-        Bike loveBike = new Bike(false, 200);
-        assertTrue((!loveBike.isOn() && loveBike.getSpeedInKmPerHour() == 200));
-        assertEquals(true, (!loveBike.isOn() && loveBike.getSpeedInKmPerHour() == 200));
+        //Bike loveBike = new Bike();
+        //loveBike.setOn(true);
+       // loveBike.setSpeedInKmPerHour(200);
+        //assertTrue((!loveBike.isOn() && loveBike.getSpeedInKmPerHour() == 200));
+       // assertEquals(true, (!loveBike.isOn() && loveBike.getSpeedInKmPerHour() == 200));
         //when
 //        okada.setSpeedInKmPerHour(15);
 //        //confirm
@@ -36,13 +45,29 @@ public class BikeTest {
     @Test
     public void testThatSpeedCannotBeSetWhenBikeIsOff(){
         //when
-        okada.setOn(false);
-        assertFalse(okada.isOn());
+        okada.setOn(true);
+        assertTrue(okada.isOn());
 
         okada.setSpeedInKmPerHour(45);
 
         assertEquals(45, okada.getSpeedInKmPerHour());
 
+    }
+    @Test
+    public void testThatBikeCanChangeGearWhenTheSpeedIncreases(){
+        //when
+        //The bike should be on
+        okada.setOn(true);
+        assertTrue(okada.isOn());
+        //set our speed in kilometre per hour
+        okada.setSpeedInKmPerHour(300);
+        assertEquals(300, okada.getSpeedInKmPerHour());
+        //set our initial gear
+        okada.setGear(5);
+        assertEquals(5,okada.getGear());
+//        //speed up our bike
+//        okada.speedUpBike();
+//        assertEquals(3, okada.getGearInMotion());
     }
 
 }
