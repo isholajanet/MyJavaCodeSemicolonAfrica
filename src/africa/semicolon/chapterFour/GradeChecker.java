d84369863c0dca1d54db28a203a1a6783842e726
Janet Ishola <janetishola2010@gmail.com>
2021-05-19 17:03:41 +0100

    my grade checker for only if

--- src/africa/semicolon/chapterFour/GradeChecker.java
+++ src/africa/semicolon/chapterFour/GradeChecker.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 /** Ask the user to enter a grade
  * Collect the user input
  * Check if the grade is 90 or above, the student get an A
- * else if the grade is greater than or equals to 80 and less than or equals to 89, the student get a B
- * else if the grade is greater than or equals to 70 and less than or equals to 79, the student get a C
- * else if the grade is greater than or equals to 60 and less than or equals to 69, the student get a D
- * else the student get a F
+ * if the grade is greater than or equals to 80 and less than or equals to 89, the student get a B
+ * if the grade is greater than or equals to 70 and less than or equals to 79, the student get a C
+ * if the grade is greater than or equals to 60 and less than or equals to 69, the student get a D
+ * if the grade is less than 60, student get a F
  *
  */
 
@@ -19,13 +19,17 @@ public class GradeChecker {
         int grade = input.nextInt();
         if (grade >= 90){
             System.out.println("A");
-        }else if (grade >= 80 && grade <= 89 ){
+        }
+        if (grade >= 80 && grade <= 89 ){
             System.out.println("B");
-        }else if (grade >= 70 && grade <= 79){
+        }
+        if(grade >= 70 && grade <= 79){
             System.out.println("C");
-        }else if (grade >= 60 && grade <= 69){
+        }
+        if (grade >= 60 && grade <= 69){
             System.out.println("D");
-        }else{
+        }
+        if (grade > 60){
             System.out.println("F");
         }
 
